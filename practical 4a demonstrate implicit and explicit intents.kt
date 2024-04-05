56355a63ca73d78ae7d5b526789d19143ea3cc53
Smooth Operator <143164739+smoothoperator2003@users.noreply.github.com>
2024-04-05 16:01:48 +0530

    Update practical 4a demonstrate implicit and explicit intents.kt

--- practical 4a demonstrate implicit and explicit intents.kt
+++ practical 4a demonstrate implicit and explicit intents.kt
@@ -1,21 +1,22 @@
-package com.example.prac4 
-import android.content.Intent 
-import android.net.Uri 
-import androidx.appcompat.app.AppCompatAcƟvity
-import android.os.Bundle 
-import android.widget.BuƩon
-class MainAcƟvity : AppCompatAcƟvity() {
- override fun onCreate(savedInstanceState: Bundle?) { 
- super.onCreate(savedInstanceState) 
- setContentView(R.layout.acƟvity_main) 
- val btn = findViewById<BuƩon>(R.id.buƩon) 
- btn.setOnClickListener() { 
- intent = Intent(Intent.ACTION_VIEW) 
- intent.setData(Uri.parse("hƩps://www.javatpoint.com/")) 
- startAcƟvity(intent) 
- /* intent= Intent(Intent.ACTION_VIEW, 
- Uri.parse("hƩps://www.javatpoint.com/"))
- startAcƟvity(intent)*/
- } 
- } 
-} 
+package com.example.prac4
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.button)
+
+        btn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://www.javatpoint.com/")
+            startActivity(intent)
+        }
+    }
+}
