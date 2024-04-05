11eb92419966ae4f04c19536aeb48efeaec69652
Smooth Operator <143164739+smoothoperator2003@users.noreply.github.com>
2024-04-05 15:49:09 +0530

    Update practical 8a MainActivity_kt implement asynctask and threading concepts.kt

--- practical 8a MainActivity_kt implement asynctask and threading concepts.kt
+++ practical 8a MainActivity_kt implement asynctask and threading concepts.kt
@@ -1,71 +1,72 @@
-package com.example.prac8a 
-import android.app.ProgressDialog 
-import android.graphics.Bitmap 
-import android.graphics.BitmapFactory 
-import android.net.wifi.WifiConfiguraƟon.AuthAlgorithm.strings 
-import android.os.AsyncTask 
-import android.os.Bundle 
-import android.support.v7.app.AppCompatAcƟvity
-import android.widget.BuƩon
-import android.widget.ImageView 
-import java.io.IOExcepƟon
-import java.io.InputStream 
-import java.net.HƩpURLConnecƟon
-import java.net.URL 
-enum class AsyncTaskExample { 
-} 
-class MainAcƟvity : AppCompatAcƟvity() {
- var ImageUrl: URL? = null 
- var `is`: InputStream? = null 
- var bmImg: Bitmap? = null 
- var imageView: ImageView? = null 
- var p: ProgressDialog? = null 
- override fun onCreate(savedInstanceState: Bundle?) { 
- super.onCreate(savedInstanceState) 
- setContentView(R.layout.acƟvity_main)
- val buƩon = findViewById<BuƩon>(R.id.asyncTask) 
- imageView = findViewById(R.id.image) 
- buƩon.setOnClickListener { 
- val asyncTask: AsyncTaskExample = this.AsyncTaskExample() { 
- } 
- } 
- abstract class AsyncTaskExample : 
- AsyncTask<String?, String?, Bitmap?>() { 
- override fun onPreExecute() { 
- super.onPreExecute() 
- p = ProgressDialog(this@MainAcƟvity) 
- p!!.setMessage("Please wait...It is downloading") 
- p!!.isIndeterminate = false 
- p!!.setCancelable(false) 
- p!!.show() 
- } 
- protected override fun doInBackground(vararg p0: String?): Bitmap? { 
- try { 
- ImageUrl = URL(strings[0]) 
- val conn = ImageUrl!!.openConnecƟon() as HƩpURLConnecƟon
- conn.doInput = true 
- conn.connect() 
- `is` = conn.inputStream 
- val opƟons = BitmapFactory.OpƟons()
- opƟons.inPreferredConfig = Bitmap.Config.RGB_565 
- bmImg = BitmapFactory.decodeStream(`is`, null, opƟons)
- } catch (e: IOExcepƟon) {
- e.printStackTrace() 
- } 
- return bmImg 
- } 
- override fun onPostExecute(bitmap: Bitmap?) { 
- super.onPostExecute(bitmap) 
- if (imageView != null) { 
- p!!.hide() 
- imageView!!.setImageBitmap(bitmap) 
- } else { 
- p!!.show() 
- } 
- } 
- } 
- } 
- private fun AsyncTaskExample(funcƟon: () -> Unit) { 
- "TODO(\"Not yet implemented\")" 
- } 
-} 
+package com.example.prac8a
+
+import android.app.ProgressDialog
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.ImageView
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+    var imageUrl: URL? = null
+    var `is`: InputStream? = null
+    var bmImg: Bitmap? = null
+    var imageView: ImageView? = null
+    var p: ProgressDialog? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.asyncTask)
+        imageView = findViewById(R.id.image)
+
+        button.setOnClickListener {
+            val asyncTask = AsyncTaskExample()
+            asyncTask.execute("Your Image URL Here")
+        }
+    }
+
+    inner class AsyncTaskExample : AsyncTask<String?, String?, Bitmap?>() {
+        override fun onPreExecute() {
+            super.onPreExecute()
+            p = ProgressDialog(this@MainActivity)
+            p!!.setMessage("Please wait...It is downloading")
+            p!!.isIndeterminate = false
+            p!!.setCancelable(false)
+            p!!.show()
+        }
+
+        override fun doInBackground(vararg p0: String?): Bitmap? {
+            try {
+                imageUrl = URL(p0[0])
+                val conn = imageUrl!!.openConnection() as HttpURLConnection
+                conn.doInput = true
+                conn.connect()
+                `is` = conn.inputStream
+                val options = BitmapFactory.Options()
+                options.inPreferredConfig = Bitmap.Config.RGB_565
+                bmImg = BitmapFactory.decodeStream(`is`, null, options)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            return bmImg
+        }
+
+        override fun onPostExecute(bitmap: Bitmap?) {
+            super.onPostExecute(bitmap)
+            if (imageView != null) {
+                p!!.dismiss()
+                imageView!!.setImageBitmap(bitmap)
+            } else {
+                p!!.show()
+            }
+        }
+    }
+}
